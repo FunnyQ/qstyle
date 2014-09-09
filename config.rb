51de78489b5d663407216d099b01bccad2e13ba3
FunnyQ <funnyq@funnyq.com>
2014-09-09 16:47:02 +0800

    adding favicon template

--- config.rb
+++ config.rb
@@ -3,7 +3,6 @@
 ###
 
 # Change Compass configuration
-# using :nested for develop & :compressed for production <==========
 compass_config do |config|
   config.output_style = :nested
   config.line_comments = false
@@ -104,7 +103,22 @@ end
 configure :build do
 
   # create favicon and touch icons
-  activate :favicon_maker
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source')
+    f.output_dir    = File.join(root, 'build')
+    f.icons = {
+      "_favicon_template_hires.png" => [
+        { icon: "apple-touch-icon-152x152-precomposed.png" },
+        { icon: "apple-touch-icon-114x114-precomposed.png" },
+        { icon: "apple-touch-icon-72x72-precomposed.png" },
+        { icon: "mstile-144x144", format: :png },
+      ],
+      "_favicon_template_lores.png" => [
+        { icon: "favicon.png", size: "16x16" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      ]
+    }
+  end
 
   # For example, change the Compass output style for deployment
   activate :minify_css
